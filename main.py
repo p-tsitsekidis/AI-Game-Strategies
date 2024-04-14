febdb372b47c36d9c0ebbe2286844a9f8127844f
Petros Tsitsekidis <p.tsitsekidis@gmail.com>
2024-04-14 22:10:50 +0300

    Created a function that simulates the games, to make the code less redundant

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from tictactoe import TicTacToe
 import randomplayer
 import minimax
+import minimax_pruning
+import time
 
-game = TicTacToe()
-
-minimax_counter = 0
-random_counter = 0
-draw_counter = 0
-
-for _ in range(100):
-    utility = game.play_game(randomplayer.random_player, minimax.minimax_player)
-    if utility == 1:
-        random_counter += 1
-    elif utility == -1:
-        minimax_counter += 1
-    else:
-        draw_counter += 1
+def simulate_games(game, player1, player2, num_games=100):
+    results = {'Player1 Wins': 0, 'Player2 Wins': 0, 'Draws': 0, 'Average Time': 0}
+    total_time = 0
+    
+    for _ in range(num_games):
+        start_time = time.time()
+        utility = game.play_game(player1, player2)
+        end_time = time.time()
         
-print(random_counter)
-print(minimax_counter)
-print(draw_counter)
+        total_time += (end_time - start_time)
+        
+        if utility == 1:
+            results['Player1 Wins'] += 1
+        elif utility == -1:
+            results['Player2 Wins'] += 1
+        else:
+            results['Draws'] += 1
+    
+    results['Average Time'] = total_time / num_games
+    return results
