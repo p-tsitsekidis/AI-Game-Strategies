abac5089cfb09bec00a9e5230fa293832d11e7d3
Petros Tsitsekidis <p.tsitsekidis@gmail.com>
2024-04-14 18:37:41 +0300

    Runs the game 100 times and counts the number of wins for each player as well as the draws

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ import minimax
 
 game = TicTacToe()
 
-utility = game.play_game(randomplayer.random_player, minimax.minimax_player)
-if utility == 1:
-    print("'X' won!")
-elif utility == -1:
-    print("'O' won!")
-else:
-    print('Tie!')
+minimax_counter = 0
+random_counter = 0
+draw_counter = 0
+
+for _ in range(100):
+    utility = game.play_game(randomplayer.random_player, minimax.minimax_player)
+    if utility == 1:
+        random_counter += 1
+    elif utility == -1:
+        minimax_counter += 1
+    else:
+        draw_counter += 1
+        
+print(random_counter)
+print(minimax_counter)
+print(draw_counter)
