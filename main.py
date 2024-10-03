915c5d9c85d5da9c8bd6a1ca2811c48f233a89fa
Petros Tsitsekidis <80098933+p-tsitsekidis@users.noreply.github.com>
2024-10-03 17:15:24 +0300

    Implement a simple command line UI to interact with the different games and players

--- main.py
+++ main.py
@@ -4,74 +4,107 @@ from randomplayer import random_player
 from manual_player import manual_player
 from minimax import minimax_player
 from minimax_pruning import minimax_pruning_player
-import time
-from reversi import Reversi
 from minimax_limited_pruning import minimax_limited_pruning_player
 from mcts import mcts_player
-import time
 
-def simulate_games(game, player1, player2, num_games=50): # Set the number of simulated games here!!
-    results = {'Player1 Wins': 0, 'Player2 Wins': 0, 'Draws': 0, 'Average Time': 0}
-    total_time = 0
+def simulate_games(game, player1, player2, num_games):
+    results = {
+        'Player1_Wins': 0,
+        'Player2_Wins': 0,
+        'Draws': 0,
+        'Average_Player1_Move_Time': 0,
+        'Average_Player2_Move_Time': 0
+        }
+    
+    total_player1_move_time = 0
+    total_player2_move_time = 0
     
     for _ in range(num_games):
-        start_time = time.time()
-        utility = game.play_game(player1, player2)
-        end_time = time.time()
+        utility, avg_player1_time, avg_player2_time = game.play_game(player1, player2)
         
-        total_time += (end_time - start_time)
+        total_player1_move_time += avg_player1_time
+        total_player2_move_time += avg_player2_time
         
         if utility == 1:
-            results['Player1 Wins'] += 1
+            results['Player1_Wins'] += 1
         elif utility == -1:
-            results['Player2 Wins'] += 1
+            results['Player2_Wins'] += 1
         else:
             results['Draws'] += 1
     
-    results['Average Time'] = total_time / num_games
+    results['Average_Player1_Move_Time'] = total_player1_move_time / num_games
+    results['Average_Player2_Move_Time'] = total_player2_move_time / num_games
     return results
 
+def game_initialization(game, player_1, player_2, num_of_games):
+    print("\n\nThe game is starting:")
+    results = simulate_games(game, player_1, player_2, num_of_games)
+    print(f"Player 1 Wins: {results['Player1_Wins']}")
+    print(f"Player 2 Wins: {results['Player2_Wins']}")
+    print(f"Draws: {results['Draws']}")
+    print(f"Average Player 1 Move Time: {results['Average_Player1_Move_Time']:.4f} seconds")
+    print(f"Average Player 2 Move Time: {results['Average_Player2_Move_Time']:.4f} seconds")
 
-# game = TicTacToe()
+all_players = {
+    1: manual_player,
+    2: random_player,
+    3: minimax_player,
+    4: minimax_pruning_player,
+    5: minimax_limited_pruning_player,
+    6: mcts_player
+}
 
-# # Testing random player vs minimax player
-# print("Testing Random Player vs Minimax Player:")
-# results_minimax = simulate_games(game, random_player, minimax_player)
-# print(f"Random Wins: {results_minimax['Player1 Wins']}")
-# print(f"Minimax Wins: {results_minimax['Player2 Wins']}")
-# print(f"Draws: {results_minimax['Draws']}")
-# print(f"Average Decision Time: {results_minimax['Average Time']:.2f} seconds")
+games = {
+    "1": TicTacToe(),
+    "2": Reversi()
+}
 
-# # Testing random player vs minimax with pruning
-# print("\nTesting Random Player vs Minimax with Pruning Player:")
-# results_minimax_pruning = simulate_games(game, random_player, minimax_pruning_player)
-# print(f"Random Wins: {results_minimax_pruning['Player1 Wins']}")
-# print(f"Minimax with Pruning Wins: {results_minimax_pruning['Player2 Wins']}")
-# print(f"Draws: {results_minimax_pruning['Draws']}")
-# print(f"Average Decision Time: {results_minimax_pruning['Average Time']:.2f} seconds")
+print("Choose which game you would like to play:")
+print("1. Tic Tac Toe")
+print("2. Reversi (Othello)")
 
-reversi = Reversi()
+while True:
+    game_choice = input("Enter your choice: ")
+    if game_choice == "1" or game_choice == "2":
+        break
+    else:
+        print("Invalid input. Please enter either '1' or '2'.")
 
-# utility = reversi.play_game(manual_player, manual_player)
-# if utility == 1:
-#     print("'X' won!")
-# elif utility == -1:
-#     print("'O' won!")
-# else:
-#     print('Tie!')
+game = games.get(game_choice)
 
-# # Testing random player vs minimax player
-# print("Testing Random Player vs Minimax Player:")
-# results_minimax = simulate_games(reversi, random_player, minimax_player)
-# print(f"Random Player 1 Wins: {results_minimax['Player1 Wins']}")
-# print(f"Random Player 2 Wins: {results_minimax['Player2 Wins']}")
-# print(f"Draws: {results_minimax['Draws']}")
-# print(f"Average Decision Time: {results_minimax['Average Time']:.2f} seconds")
+for i in range(2):
+    print(f"\n\nChoose Player {i+1}:")
+    print("1. Human Player (You)")
+    print("2. Random Player")
+    print("3. Minimax Player")
+    print("4. Minimax with Pruning Player")
+    print("5. Minimax with Limited Pruning Player")
+    print("6. Monte Carlo Player")
 
-# Testing random player vs minimax with pruning
-print("\nTesting Random Player vs Minimax with Pruning Player:")
-results_minimax_pruning = simulate_games(reversi, random_player, minimax_limited_pruning_player)
-print(f"Random Wins: {results_minimax_pruning['Player1 Wins']}")
-print(f"Minimax with Pruning Wins: {results_minimax_pruning['Player2 Wins']}")
-print(f"Draws: {results_minimax_pruning['Draws']}")
-print(f"Average Decision Time: {results_minimax_pruning['Average Time']:.2f} seconds")
+    while True:
+        try:
+            player_choice = int(input("Enter your choice: "))
+            if 1 <= player_choice <= 6:
+                break
+            else:
+                print("Invalid input. Please enter a single number that corresponds to your choice.")
+        except ValueError:
+            print("Invalid input. Please enter a single number that corresponds to your choice.")
+
+    if i == 0:
+        player_1 = all_players.get(player_choice)
+    else:
+        player_2 = all_players.get(player_choice)
+
+print("Enter the number of games you wish to play (Statistics are shown at the end)")
+while True:
+    try:
+        num_of_games = int(input("Enter a positive integer: "))
+        if num_of_games > 0:
+            break
+        else:
+            print("Invalid number. Please enter a positive integer that reflects the number of games you wish to play.")
+    except ValueError:
+        print("Invalid input. Please enter a positive integer that reflects the number of games you wish to play.")
+
+game_initialization(game, player_1, player_2, num_of_games)
